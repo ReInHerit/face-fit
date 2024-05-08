12776f3f9e1fe125d885b34ef1703eef0ae9be0b
Marco Bertini <marco.bertini@unifi.it>
2024-05-08 17:20:57 +0200

    FIX: improved serving of /media from services like Heroku. See https://jaintle.medium.com/how-to-deploy-a-django-application-on-heroku-3923fd6c51bf for more info

--- faceFit_django/FaceFit/urls.py
+++ faceFit_django/FaceFit/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from . import views
 
@@ -12,8 +13,11 @@ urlpatterns = [
     path('delete_morphs/', views.delete_morphs, name='delete_morphs'),
     path('get_dataset/', views.get_dataset, name='get_dataset'),
     path('policy/', views.policy, name='policy'),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
